205672a6e039761d62327e126230154cb44aff63
MaestroD <daniiltepl@gmail.com>
2022-11-11 22:30:50 +0100

    Adjusted Clustering File
    -Made Clustering a class and added progress bar
    -Added Visualization File

---
+++ src/Clustering/ClusterVisualization.py
@@ -0,0 +1,6 @@
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap, LogNorm
+
+def visulize_clusters():
+    return
--- src/Clustering/Clustering.py
+++ src/Clustering/Clustering.py
@@ -2,27 +2,7 @@ from PhaseEstimation.vqe import *
 import pennylane as qml
 from jax import random
 import jax.numpy as jnp
-
-vqe = load_vqe("../../data/vqes/ANNNI/N6n10")
-
-
-def compute_clusters(centroids: jnp.ndarray, states: jnp.ndarray) -> List[List[int]]:
-    clusters = [[] for _ in centroids]
-
-    if len(centroids) > 2:
-        prog = 1
-        for state_index in range(len(states)):
-            closest, index = fidelity(centroids[0], states[state_index])[0], 0
-            for i in range(1, len(centroids)):
-                similarity = fidelity(centroids[i], states[state_index])[0]
-                if similarity > closest:
-                    closest, index = similarity, i
-            clusters[index].append(state_index)
-            print(prog / len(states))
-            prog += 1
-        return clusters
-    else:
-        print("There must be at least 2 centroids to cluster!")
+import progressbar
 
 
 def compute_mean(clusters: List, hamiltonian: qml.ops.qubit.hamiltonian.Hamiltonian) -> list[jnp.ndarray]:
@@ -34,6 +14,13 @@ def compute_mean(clusters: List, hamiltonian: qml.ops.qubit.hamiltonian.Hamilton
     return cluster_means
 
 
+def find_nearest_state(hamiltonian: qml.ops.qubit.hamiltonian.Hamiltonian, mean_arr: jnp.ndarray) -> int:
+    diff_array = jnp.abs(hamiltonian.model_params - mean_arr)
+    sum_diff_array = jnp.array([row[1] + row[2] for row in diff_array])
+    idx = sum_diff_array.argmin()
+    return idx
+
+
 def compute_new_centroids_from_existing_states(mean_params: list[jnp.ndarray],
                                                hamiltonian: qml.ops.qubit.hamiltonian.Hamiltonian, states: jnp.ndarray):
     new_centroids = []
@@ -43,30 +30,71 @@ def compute_new_centroids_from_existing_states(mean_params: list[jnp.ndarray],
     return new_centroids
 
 
-def find_nearest_state(hamiltonian: qml.ops.qubit.hamiltonian.Hamiltonian, mean_arr: jnp.ndarray) -> int:
-    diff_array = jnp.abs(hamiltonian.model_params - mean_arr)
-    sum_diff_array = jnp.array([row[1] + row[2] for row in diff_array])
-    idx = sum_diff_array.argmin()
-    return idx
+class ClusteringVQE:
+    def __init__(self, path_to_file, num_clusters, iterations, show_progress=True):
+        self.vqe = load_vqe(path_to_file)
+        self.num_clusters = num_clusters
+        self.iterations = iterations
+        self.clusters = [[] for i in range(num_clusters)]
+
+        # For Progress
+        self.progress = 0
+        self.show_progress = show_progress
+        self.widgets = [' [', progressbar.Timer(format='elapsed time: %(elapsed)s'), '] ', progressbar.AnimatedMarker(),
+                        progressbar.Bar('*'), ' (',
+                        progressbar.ETA(), ') ', 'CLusters: ',
+                        progressbar.FormatLabel(str([len(cluster) for cluster in self.clusters]))]
+        self.bar = progressbar.ProgressBar(maxval=self.iterations * len(self.vqe.vqe_params0) * self.num_clusters,
+                                           widgets=self.widgets)
+
+        @qml.qnode(self.vqe.device, interface="jax")
+        def fidelity(params_phi, params_psi):
+            qml.adjoint(self.vqe.circuit)(params_phi)
+            self.vqe.circuit(params_psi)
+
+            if self.show_progress:
+                self.progress += 1
+                self.bar.update(self.progress)
+            return qml.probs(wires=[i for i in range(self.vqe.Hs.N)])  # Only interested in |000...0>
+
+        self.fidelity = fidelity
+
+    def compute_clusters(self, centroids: jnp.ndarray, states: jnp.ndarray) -> List[List[int]]:
+        clusters = [[] for _ in centroids]
+
+        if len(centroids) > 2:
+            for state_index in range(len(states)):
+                closest, index = self.fidelity(centroids[0], states[state_index])[0], 0
+                for i in range(1, len(centroids)):
+                    similarity = self.fidelity(centroids[i], states[state_index])[0]
+                    if similarity > closest:
+                        closest, index = similarity, i
+                clusters[index].append(state_index)
+
+                if self.show_progress:
+                    self.widgets[len(self.widgets) - 1] = progressbar.FormatLabel(
+                        str([len(cluster) for cluster in clusters]))
+            return clusters
+        else:
+            print("There must be at least 2 centroids to cluster!")
+
+    def cluster(self):
+        # Initialize centroids randomly
+        centroids = random.choice(random.PRNGKey(0), self.vqe.vqe_params0, (self.num_clusters,))
+        if self.show_progress:
+            self.progress = 0
+            self.bar.start()
+
+        for i in range(self.iterations):
+            clusters = self.compute_clusters(centroids, self.vqe.vqe_params0)
+            mean_params = compute_mean(clusters, self.vqe.Hs)
+            centroids = compute_new_centroids_from_existing_states(mean_params, self.vqe.Hs, self.vqe.vqe_params0)
+            self.clusters = clusters
+
+        if self.show_progress:
+            self.bar.finish()
 
 
-def repeat_clustering(variational_quantum_eigensolver: vqe, num_clusters: int, iterations: int):
-    # Initialize centroids randomly
-    centroids = random.choice(random.PRNGKey(0), vqe.vqe_params0, (num_clusters,))
-
-    for i in range(iterations):
-        clusters = compute_clusters(centroids, variational_quantum_eigensolver.vqe_params0)
-        mean_params = compute_mean(clusters, variational_quantum_eigensolver.Hs)
-        centroids = compute_new_centroids_from_existing_states(mean_params, variational_quantum_eigensolver.Hs,
-                                                               variational_quantum_eigensolver.vqe_params0)
-        print([len(cluster) for cluster in clusters])
-
-
-@qml.qnode(vqe.device, interface="jax")
-def fidelity(params_phi, params_psi):
-    qml.adjoint(vqe.circuit)(params_phi)
-    vqe.circuit(params_psi)
-    return qml.probs(wires=[i for i in range(vqe.Hs.N)])  # Only interested in |000...0>
-
-
-repeat_clustering(vqe, 3, 10)
+ClusteringVQEObj = ClusteringVQE("../../data/vqes/ANNNI/N6n10", 3, 5)
+ClusteringVQEObj.cluster()
+print(ClusteringVQEObj.clusters)
