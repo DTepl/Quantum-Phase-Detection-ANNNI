ade054194e19cdc36ea0925c20afc50203fae399
MaestroD <daniiltepl@gmail.com>
2022-11-13 22:19:08 +0100

    Adjusted Visualization
    - Made visualization in heatmap

--- src/Clustering/ClusterVisualization.py
+++ src/Clustering/ClusterVisualization.py
@@ -15,19 +15,33 @@ def visualize_clusters(clusters: List[List], hamiltonians: qml.ops.qubit.hamilto
         ["lightcoral", "skyblue", "black", "palegreen"]
     )
     norm = mpl.colors.BoundaryNorm(jnp.arange(0, 4), phases.N)
+    side = int(jnp.sqrt(hamiltonians.n_states))
 
-    sc = []
+    cluster_visual = [i for i in range(hamiltonians.n_states)]
 
     for i, cluster in enumerate(clusters):
-        params = hamiltonians.model_params[cluster]
-        h = params[:, 1]
-        k = params[:, 2]
-        c = [i for _ in range(len(h))]
-        sc = plt.scatter(k, h, c=c, cmap=phases, norm=norm)
+        for index in cluster:
+            cluster_visual[index] = i
+
+    sc = plt.imshow(jnp.rot90(jnp.reshape(jnp.array(cluster_visual), (side, side))), cmap=phases, norm=norm)
 
     plt.ylabel(r"$h$", fontsize=24)
     plt.xlabel(r"$\kappa$", fontsize=24)
+
+    plt.xticks(
+        ticks=jnp.linspace(0, side - 1, 5).astype(int),
+        labels=[jnp.round(k * 1 / 4, 2) for k in range(0, 5)],
+        fontsize=18,
+    )
+    plt.yticks(
+        ticks=jnp.linspace(0, side - 1, 5).astype(int),
+        labels=[jnp.round(k * 2 / 4, 2) for k in range(4, -1, -1)],
+        fontsize=18,
+    )
     plt.colorbar(sc)
+    plt.savefig(
+        "../../data/clustering/clustering_meaningParams_N" + str(hamiltonians.N) + "-n" + str(
+            hamiltonians.n_states) + ".png")
     plt.show()
 
 
